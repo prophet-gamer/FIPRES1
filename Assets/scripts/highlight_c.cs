d0bdb8182325b996805a19b21556462174e80e6a
prophet-gamer <alirezachenary@gmail.com>
2023-08-21 05:10:32 +0330

    forth

--- Assets/scripts/highlight_c.cs
+++ Assets/scripts/highlight_c.cs
@@ -3,10 +3,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class highlight_c : MonoBehaviour
 {
+    /*
     public Camera camera;
     private int DefaultLayer;
     private int HighlightLayer;
@@ -43,5 +46,47 @@ public class highlight_c : MonoBehaviour
             currentTarget = null;
         }
         
+    }*/
+    private Transform highlight;
+    private Transform selection;
+    private RaycastHit raycastHit;
+    private Vector3 Center;
+
+    void Start()
+    {
+        Center = new Vector3(Screen.width >> 1, Screen.height >> 1);
+    }
+
+    void Update()
+    {
+        // Highlight
+        if (highlight != null)
+        {
+            highlight.gameObject.GetComponent<Outline>().enabled = false;
+            highlight = null;
+        }
+        Ray ray = Camera.main.ScreenPointToRay(Center);
+        if (Physics.Raycast(ray, out raycastHit)) //Make sure you have EventSystem in the hierarchy before using EventSystem
+        {
+            highlight = raycastHit.transform;
+            if ((highlight.CompareTag("interact") || highlight.CompareTag("fuse") || highlight.CompareTag("Open") || highlight.CompareTag("lever")) && highlight != selection)
+            {
+                if (highlight.gameObject.GetComponent<Outline>() != null)
+                {
+                    highlight.gameObject.GetComponent<Outline>().enabled = true;
+                }
+                else
+                {
+                    Outline outline = highlight.gameObject.AddComponent<Outline>();
+                    outline.enabled = true;
+                    highlight.gameObject.GetComponent<Outline>().OutlineColor = Color.magenta;
+                    highlight.gameObject.GetComponent<Outline>().OutlineWidth = 10.0f;
+                }
+            }
+            else
+            {
+                highlight = null;
+            }
+        }
     }
 }
